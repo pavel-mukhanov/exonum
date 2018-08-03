471a33c548234942e98d97e7df7707f35eb13fbe
Pavel Mukhanov <mukhanovpv@gmail.com>
2018-08-03 10:42:27 +0300

    Group imports.

--- exonum/src/storage/migration.rs
+++ exonum/src/storage/migration.rs
@@ -1,14 +1,27 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-use storage::base_index::BaseIndex;
-use storage::indexes_metadata;
-use storage::Fork;
-use storage::Iter;
-use storage::ListIndex;
-use storage::Patch;
-use storage::Snapshot;
-use storage::StorageKey;
-use storage::StorageValue;
+// Copyright 2018 The Exonum Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! An implementation of an array list of items.
+//!
+//! The given section contains methods related to `ListIndex` and the iterator
+//! over the items of this list.
+
+use std::{cell::RefCell, rc::Rc};
+use storage::{
+    base_index::BaseIndex, indexes_metadata, Fork, Iter, ListIndex, Patch, Snapshot, StorageKey,
+    StorageValue,
+};
 
 #[derive(Debug, Clone)]
 pub struct MigrationFork {
